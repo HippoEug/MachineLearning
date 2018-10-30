ae28bf88fb9b747267a0be8fbee6bef3f8356071
Eugene <efoo10@gmail.com>
2018-10-30 16:39:45 +0800

    Added feature scaling

--- 1DataPreprocessing/data_preprocessing_template.py
+++ 1DataPreprocessing/data_preprocessing_template.py
@@ -30,3 +30,9 @@ y = labelencoder_y.fit_transform(y) # Encode Yes as 1, No as 0
 # Splitting dataset into training and test sets
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0) # X_train is the training part of matrix of features, y_train is the training part of the dependent variables
+
+# Feature scaling to prevent domination in ie Euclidean algorithm
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train) # Scale X_train, fit and transform for training set
+X_test = sc_X.transform(X_test) # Scale X_test, transform for test set only
