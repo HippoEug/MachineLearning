e9b80d4748610b55bbf976d82d77cf3006fc6919
Eugene <efoo10@gmail.com>
2018-10-30 13:40:06 +0800

    Added encoding and split dataset to training and test sets

--- 1DataPreprocessing/data_preprocessing_template.py
+++ 1DataPreprocessing/data_preprocessing_template.py
@@ -15,3 +15,18 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0) # Object imputer with parameters
 imputer = imputer.fit(X[:, 1:3]) # Fit the missing data in all rows, and columns 1 and 2
 X[:, 1:3] = imputer.transform(X[:, 1:3]) # Replace missing data by the mean of the column
+
+# Enconding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0]) # Fit label encoder to countries, encoded by replacing text with number
+
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y) # Encode Yes as 1, No as 0
+
+# Splitting dataset into training and test sets
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0) # X_train is the training part of matrix of features, y_train is the training part of the dependent variables
