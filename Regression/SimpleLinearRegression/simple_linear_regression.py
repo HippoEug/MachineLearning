d3935481a7a8e3f8749602c4fbad0f93ae62f8c4
Eugene <efoo10@gmail.com>
2018-10-31 11:34:30 +0800

    Added regression, predicted and visualized results

--- Regression/SimpleLinearRegression/simple_linear_regression.py
+++ Regression/SimpleLinearRegression/simple_linear_regression.py
@@ -12,12 +12,28 @@ y = dataset.iloc[:, 1].values # Dependent Variable, Salary
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+# Fitting Linear Regression to Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting Test set results
+y_pred = regressor.predict(X_test)
+
+# Visualizing training set results
+plt.scatter(X_train, y_train, color = 'red') # X = Years of Experience, Y = Predicted Salary
+plt.plot(X_train, regressor.predict(X_train), color = 'blue') # X = Years of Experience, Y = Predicted Salary
+plt.title('Salary VS Experience (Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualizing test set results
+plt.scatter(X_test, y_test, color = 'orange') # X = Years of Experience, Y = Predicted Salary
+plt.plot(X_train, regressor.predict(X_train), color = 'blue') # X = Years of Experinece, Y = Predicted Salary
+plt.title('Salary VS Experience (Test Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
